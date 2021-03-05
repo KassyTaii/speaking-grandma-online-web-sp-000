60adae4e70a4595ee61876e909427834cf0a8ace
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-03-05 01:05:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I OVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
  elseif phrase == upcase
  puts "NO, NOT SINCE 1938!"
  else phrase == "I LOVE YOU GRANDMA!"
